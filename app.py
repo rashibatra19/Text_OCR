8ead4f2de89fab720a45fb5eac4b180b746fe669
unknown <rashi.bce21@sot.pdpu.ac.in>
2024-09-30 02:10:07 +0530

    Commit

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ import tempfile
 @st.cache_resource
 def load_model():
     tokenizer = AutoTokenizer.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True)
-    model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, device_map='cuda', use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
-    model = model.eval().cuda()
+    model = AutoModel.from_pretrained('ucaslcl/GOT-OCR2_0', trust_remote_code=True, low_cpu_mem_usage=True, use_safetensors=True, pad_token_id=tokenizer.eos_token_id)
+    model = model.eval()
     return tokenizer, model
 
 def search_keyword(extracted_text, keyword):
